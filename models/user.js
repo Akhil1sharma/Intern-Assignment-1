858add5fd22723434a243cded2a00b320897f02f
Akhil1sharma <168325399+Akhil1sharma@users.noreply.github.com>
2025-03-25 11:18:46 +0530

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  role: { type: String, enum: ['student', 'professor'] },
+  password: String,
+  availability: [{ type: Date }],
+});
+
+userSchema.pre('save', async function(next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 8);
+  }
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
